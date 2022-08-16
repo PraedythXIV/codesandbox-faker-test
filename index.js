4a1a18b5ea644bdaf82878935559e0e518f4d9b2
Praedy <praedythxvi@gmail.com>
2022-08-16 14:21:58 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let passengersSql = `
 
 20 ** 36
 
-for (let i = 0; i < 100 - 2; i++) {
+for (let i = 0; i < 25; i++) {
   const gender = faker.name.gender(true)
   const country = faker.address.countryCode()
   passengersSql += `(
@@ -54,7 +54,59 @@ for (let i = 0; i < 100 - 2; i++) {
     ),`
 }
 
-writeFileSync("./data.sql", passengersSql.slice(0, -1), {
+writeFileSync("./dataPassengers.sql", passengersSql.slice(0, -1), {
+  encoding: "utf-8",
+  flag: "w",
+})
+
+// crewMembers
+let crewMembersSql = `
+    INSERT INTO "crewMembers" (
+        "passportId",
+        "gender",
+        "firstName",
+        "lastName",
+        "birthDate",
+        "birthCountry",
+        "citizenshipCountry",
+        "addressLine1",
+        "addressLine2",
+        "zipcode",
+        "city",
+        "country"
+    ) VALUES
+`
+
+20 ** 36
+
+for (let i = 0; i < 25; i++) {
+  const gender = faker.name.gender(true)
+  const country = faker.address.countryCode()
+  crewMembersSql += `(
+        '${faker.random.alphaNumeric(20).toUpperCase()}',
+        '${gender}',
+        '${faker.name.firstName(gender).replace(/'/g, "''")}',
+        '${faker.name.lastName(gender).replace(/'/g, "''")}',
+        '${new Date(faker.date.past(faker.random.numeric(2))).toISOString()}',
+        '${country}',
+        '${
+          Math.floor(Math.random() * 100) % 40
+            ? country
+            : faker.address.countryCode()
+        }',
+        '${faker.address.streetAddress().replace(/'/g, "''")}',
+        '${faker.address.secondaryAddress().replace(/'/g, "''")}',
+        '${faker.address.zipCode()}',
+        '${faker.address.city().replace(/'/g, "''")}',
+        '${
+          Math.floor(Math.random() * 100) % 80
+            ? country
+            : faker.address.countryCode()
+        }'
+    ),`
+}
+
+writeFileSync("./dataCrewMembers.sql", crewMembersSql.slice(0, -1), {
   encoding: "utf-8",
   flag: "w",
 })
