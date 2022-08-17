7c823cda2622d8e6c2b64ce23156c65823d33244
Praedy <praedythxvi@gmail.com>
2022-08-17 08:24:43 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -73,7 +73,9 @@ let crewMembersSql = `
         "addressLine2",
         "zipcode",
         "city",
-        "country"
+        "country",
+        "jobType",
+        "jobTitle"
     ) VALUES
 `
 
@@ -102,11 +104,68 @@ for (let i = 0; i < 25; i++) {
           Math.floor(Math.random() * 100) % 80
             ? country
             : faker.address.countryCode()
-        }'
+        }',
+        '${faker.name.jobType()}',
+        '${faker.name.jobTitle()}'
     ),`
 }
 
 writeFileSync("./dataCrewMembers.sql", crewMembersSql.slice(0, -1), {
+  encoding: "utf-8",
+  flag: "w",
+})
+
+// companies
+let companiesSql = `
+    INSERT INTO "companies" (
+        "name",
+        "isLowCost"
+    ) VALUES
+`
+
+for (let i = 0; i < 500; i++) {
+  companiesSql += `(
+        '${faker.unique(faker.company.name).replace(/'/g, "''")}',
+        ${Math.floor(Math.random() * 100) < 57 ? "TRUE" : "FALSE"}
+    ),`
+}
+
+writeFileSync("./dataCompanies.sql", companiesSql.slice(0, -1), {
+  encoding: "utf-8",
+  flag: "w",
+})
+
+// airport
+let airportSql = `
+    INSERT INTO "airport" (
+        "name",
+        "addressLine1",
+        "addressLine2",
+        "zipcode",
+        "city",
+        "country"
+    ) VALUES
+`
+
+20 ** 36
+
+for (let i = 0; i < 25; i++) {
+  const country = faker.address.countryCode()
+  airportSql += `(
+        '${faker.name.firstName().replace(/'/g, "''")}',
+        '${faker.address.streetAddress().replace(/'/g, "''")}',
+        '${faker.address.secondaryAddress().replace(/'/g, "''")}',
+        '${faker.address.zipCode()}',
+        '${faker.address.city().replace(/'/g, "''")}',
+        '${
+          Math.floor(Math.random() * 100) % 80
+            ? country
+            : faker.address.countryCode()
+        }'
+    ),`
+}
+
+writeFileSync("./dataAirports.sql", airportSql.slice(0, -1), {
   encoding: "utf-8",
   flag: "w",
 })
